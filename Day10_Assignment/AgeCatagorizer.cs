a878e13bc5ea381c18ba05a94693c45d0f3ab43a
MansiRavindraMohan <manasimohan29@gmail.com>
2023-07-26 11:43:10 +0530

    [Mansi]: Program for person's age determination

---
+++ Day10_Assignment/AgeCatagorizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_Assignment
+{
+    public class AgeCatagorizer
+    {
+        public static string GetAgeCategory(int age)
+        {
+            if (age >= 1 && age <= 14)
+                return "Children";
+            else if (age >= 15 && age <= 24)
+                return "Youth";
+            else if (age >= 25 && age <= 64)
+                return "Adults";
+            else if (age >= 65)
+                return "Seniors";
+            else
+                throw new InvalidAgeException("Invalid age entered.");
+        }
+    }
+}
---
+++ Day10_Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the person's age: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                    throw new InvalidAgeException("Age cannot be empty or null.");
+
+                if (int.TryParse(input, out int age))
+                {
+                    string category = AgeCatagorizer.GetAgeCategory(age);
+                    Console.WriteLine($"Age category: {category}");
+                }
+                else
+                {
+                    throw new InvalidAgeException("Invalid age format. Please enter a valid number.");
+                }
+            }
+            catch (InvalidAgeException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
